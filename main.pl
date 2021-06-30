2a0616c6d848df8f9576266db32f1afcf2b46d86
Nolan Taft <killer123yo@gmail.com>
2021-06-30 00:41:57 -0600

    Create main.pl

---
+++ main.pl
@@ -0,0 +1,14 @@
+% Fruits - Simple Program
+
+% Colors
+orange(orange).
+orange(apricot).
+orange(cantaloupe).
+orange(mango).
+
+blue(blueberries).
+blue(elderberries).
+
+red(cherries).
+red(apples).
+red(strawberries).
